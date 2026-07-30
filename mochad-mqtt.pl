269990d98bda83a2e1067e102d0de492e3edb357
notsointerim <50243255+notsointerim@users.noreply.github.com>
2026-07-30 10:25:16 -0400

    Update mochad-mqtt.pl

--- mochad-mqtt.pl
+++ mochad-mqtt.pl
@@ -774,13 +774,13 @@ sub process_security_line {
 
     my $key   = 'SEC' . normalize_secaddr($addr);
     my $state = secfunc_to_state($func);
-	AE::log error => "security func: $func (addr $addr)";
+
     unless ( defined $state ) {
         AE::log info => "Unhandled security func: $func (addr $addr)";
         return;
     }
 
-    AE::log error => "processing security $key: $func => $state";
+    AE::log info => "processing security $key: $func => $state";
 
     if ( $ignore{$key} ) {
         AE::log debug => "ignoring security device $key";
@@ -795,12 +795,12 @@ sub process_security_line {
 
     my $alias = defined $devcodes{$key} ? $devcodes{$key} : lc $key;
     $status{'alias'} = $alias if ( defined $devcodes{$key} );
-	
-	AE::log error => "mqtt_status $alias: $func => $status";
+
     send_mqtt_status( $alias, \%status );
     save_state( $alias, \%status );
     store_state();
 }
+
 
 sub hass_publish_all() {
     for my $alias ( sort( keys %alias ) ) {
